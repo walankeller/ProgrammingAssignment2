9032bd08195c4b2a415677a9e98302e3a6096b2d
Alan Keller <walankeller@gmail.com>
2014-04-27 22:49:58 -0500

    added hw instructions at end of script file
    added hw instructions at end of script file

--- cachematrix.R
+++ cachematrix.R
@@ -53,4 +53,12 @@ cacheSolve <- function(x, ...) {
 # # original matrix
 # cx$get()                                  
 # # matrix inverse
-# cacheSolve(cx)                            
+# cacheSolve(cx)      
+
+
+# adding the following hw assignment instructions:
+# 1)Fork the GitHub repository containing the stub R files at https://github.com/rdpeng/ProgrammingAssignment2 to create a copy under your own account.
+# 2)Clone your forked GitHub repository to your computer so that you can edit the files locally on your own machine.
+# 3)Edit the R file contained in the git repository and place your solution in that file (please do not rename the file).
+# 4)Commit your completed R file into YOUR git repository and push your git branch to the GitHub repository under your account.
+# 5)Submit to Coursera the URL to your GitHub repository that contains the completed R code for the assignment.
